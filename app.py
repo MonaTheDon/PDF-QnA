53354a2cd255c6b7ac677ca375f7aa3883f9bfc3
MonaTheDon <e21cseu0656@bennett.edu.in>
2024-01-16 22:44:06 +0530

    conversation change

--- app.py
+++ app.py
@@ -3,14 +3,18 @@ from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import FAISS
-
+from langchain.embeddings import HuggingFaceInstructEmbeddings
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI
 
 def get_pdf_text(pdf_docs):
     text=""
-    for pdf in pdf_docs:
-        pdf_reader=PdfReader(pdf)
-        for page in pdf_reader.pages:
-            text+=page.extract_text()
+    pdf_reader=PdfReader(pdf_docs)
+    # for pdf in pdf_docs:
+    #     pdf_reader=PdfReader(pdf)
+    for page in pdf_reader.pages:
+        text+=page.extract_text()
     return text
 
 def get_chunks(raw_text):
@@ -24,14 +28,29 @@ def get_chunks(raw_text):
     return chunks
 
 def get_vector_store(chunks):
-    embeddings=abcd
+    embeddings=HuggingFaceInstructEmbeddings(model_name='hkunlp/instructor-xl')
     vectorstore=FAISS.from_texts(texts=chunks,embedding=embeddings)
     return vectorstore
+
+def get_conversation_chain(vector_store):
+    llm=ChatOpenAI()
+    memory=ConversationBufferMemory(memory_key='chat_history',return_messages= True)
+    conversation_chain=ConversationalRetrievalChain.from_llm(llm=llm,
+        retriever=vector_store.as_retriever(),
+        memory=memory)
+    
+    return conversation_chain
+
+
+
 
 
 def main():
     load_dotenv()
     st.set_page_config(page_title="QnA Model with Multiple PDFs", page_icon=":books:")
+    #if app runs itself then convo is initialized it will not re-initialize it, so we can use it anytime in the program (the var is persistent)
+    if "conversation" not in st.session_state:
+        st.session_state.conversation=None
 
     st.header("QnA Model with Multiple PDFs :books:")
     st.text_input("Ask a Question about your documents:")
@@ -47,7 +66,9 @@ def main():
                 text_chunks=get_chunks(raw_text)
                 #create vector store
                 vector_store=get_vector_store(text_chunks)
-
+                #create convo chain
+                st.session_state.conversation=get_conversation_chain(vector_store)
+    
 
 if __name__=='__main__':
     main()
